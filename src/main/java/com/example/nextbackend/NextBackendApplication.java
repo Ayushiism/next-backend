81e32f6dd6f09d65e92e94db03a5075be0913c20
Ayushiiism <ayushtripathy08@gmail.com>
2023-09-26 15:12:44 +0530

    first commit

---
+++ src/main/java/com/example/nextbackend/NextBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nextbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nextbackend/NextBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
